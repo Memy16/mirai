cd4f3a1294749d14450b470d419809a7ae3bdbee
Emmy <emmymachadodeoliveira16@gmail.com>
2025-07-18 19:00:15 -0300

    Funcionalidad de menú hamburguesa para vista responsive añadida

---
+++ JS/navbar.js
@@ -0,0 +1,10 @@
+// JavaScript para manejar el menú hamburguesa
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarToggle = document.getElementById('navbarToggle');
+    const navbarMenu = document.getElementById('navbarColor03');
+    
+    navbarToggle.addEventListener('click', () => {
+        navbarMenu.classList.toggle('collapse'); 
+    });
+});
