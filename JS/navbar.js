ee0fce298d1bb2cbeb03511d702d91924a0ad2f8
Emmy <emmymachadodeoliveira16@gmail.com>
2025-08-25 21:05:17 -0300

    Funcionalidad de modo oscuro añadida

--- JS/navbar.js
+++ JS/navbar.js
@@ -8,3 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
         navbarMenu.classList.toggle('collapse'); 
     });
 });
+
+// Modo claro y oscuro
+
+const lightBtn = document.querySelector('.light_mode');
+
+    lightBtn.addEventListener('click', () => {
+      document.body.classList.toggle('dark-mode');
+
+      const img = lightBtn.querySelector('img');
+      if(document.body.classList.contains('dark-mode')) {
+        img.src = './assets/img/modo-claro.png';
+      } else {
+        img.src = './assets/img/modo-oscuro.png';
+      }
+    });
